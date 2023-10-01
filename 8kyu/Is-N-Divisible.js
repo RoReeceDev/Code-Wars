d08ba5a0f509884d53f4c9249fa283aa96f7f5ce
RoReeceDev <roreecedev@gmail.com>
2023-09-30 22:25:53 -0400

    updated files

--- 8kyu/Is-N-Divisible.js
+++ 8kyu/Is-N-Divisible.js
@@ -13,3 +13,18 @@ function isDivisible(n, x, y) {
       return false
     }
   }
+
+// Sample Test
+
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold=0;
+
+describe("Basic tests", () => {
+  it("Fixed Tests", () => {
+    assert.strictEqual(isDivisible(3,3,4),false);
+    assert.strictEqual(isDivisible(12,3,4),true);
+    assert.strictEqual(isDivisible(8,3,4),false);
+    assert.strictEqual(isDivisible(48,3,4),true);
+  });
+});
