340738e1f31720583b99f408159bbd554d32dd49
RoReeceDev <roreecedev@gmail.com>
2023-10-06 23:02:53 -0400

    Daily Code Wars 10.6.23

---
+++ 8kyu/convert-to-a-string.js
@@ -0,0 +1,22 @@
+/*We need a function that can transform a number (integer) into a string.
+
+What ways of achieving this do you know?
+
+Examples (input --> output):
+123  --> "123"
+999  --> "999"
+-100 --> "-100"*/
+
+function numberToString(num) {
+    // Return a string of the number here!
+    return `${num}`
+}
+
+//Sample Tests
+
+const assert = require('chai').assert;
+describe("Tests", () => {
+  it("test", () => {
+    assert.strictEqual(numberToString(67), '67');
+  });
+});
